d7d3889ba85283f90b890dbc83691f19c5366a5b
Radu-nrd <radudaniel.nistor@ulbsibiu.ro>
2023-12-17 16:10:29 +0200

    Added log in and usergroups part2

--- src/main/java/com/parking/parkinglot/ejb/UserBean.java
+++ src/main/java/com/parking/parkinglot/ejb/UserBean.java
@@ -4,14 +4,17 @@ import com.parking.parkinglot.common.CarDto;
 import com.parking.parkinglot.common.UserDto;
 import com.parking.parkinglot.entities.Car;
 import com.parking.parkinglot.entities.User;
+import com.parking.parkinglot.entities.UserGroup;
 import jakarta.ejb.EJBException;
 import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
 import jakarta.jws.soap.SOAPBinding;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -41,6 +44,30 @@ public class UserBean {
         }
         return list;
 
+    }
+
+    @Inject
+    PasswordBean passwordBean;
+    public void createUser(String username, String email, String password, Collection<String> groups){
+        LOG.info("createUser");
+
+        User newUser=new User();
+        newUser.setUsername(username);
+        newUser.setEmail(email);
+        newUser.setPassword(passwordBean.convertToSha256(password));
+        entityManager.persist(newUser);
+
+        assignGroupsToUser(username, groups);
+    }
+    private void assignGroupsToUser(String username, Collection<String>groups){
+        LOG.info("assignGroupsToUser");
+
+        for(String group:groups) {
+            UserGroup userGroup=new UserGroup();
+            userGroup.setUsername(username);
+            userGroup.setUserGroup(group);
+            entityManager.persist(userGroup);
+        }
     }
 
 
